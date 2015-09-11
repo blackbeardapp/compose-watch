b10086f47aba47b8859141a2c239be181a27d143
Kevin Simper <kevin.simper@gmail.com>
2015-09-11 16:00:24 +0200

    Look up the docker-compose.yml in different folders

--- cli.js
+++ cli.js
@@ -1,5 +1,8 @@
 var argv = require('optimist').argv
 var nodemon = require('nodemon')
+var path = require('path')
+var fs = require('fs')
+var debug = require('debug')('compose-watch')
 
 if(argv._.length !== 1) {
   var help = ['Usage: compose-watch appname']
@@ -7,5 +10,35 @@ if(argv._.length !== 1) {
 }
 
 if(argv._.length === 1) {
-  nodemon('--exec "docker-compose stop -t 0 ' + argv._ + ' && docker-compose start ' + argv._ + ' && docker-compose logs ' + argv._ + '" -w ' + argv._ + '/')  
+  var dockerComposeYaml = findFile('docker-compose.yml', process.cwd())
+  var app = argv._[0]
+  var command = [
+    '--exec "',
+    'docker-compose -f ' + dockerComposeYaml + ' stop -t 0 ' + app + ' && ',
+    'docker-compose -f ' + dockerComposeYaml + ' start ' + app + ' && ',
+    'docker-compose -f ' + dockerComposeYaml + ' logs ' + app + '"',
+    ' -w ' + app + '/'
+  ].join('')
+  debug('command', command)
+  nodemon(command)
+}
+
+var prev = ''
+
+function findFile(name, dir) {
+  var filepath = path.resolve(dir, name)
+
+  // we hit root /
+  if(prev === filepath) {
+    return false
+  }
+  prev = filepath
+
+  debug('checking', filepath)
+  try {
+    fs.statSync(filepath)
+    return filepath
+  } catch (e) {
+    return findFile(name, dir + '/..')
+  }
 }
