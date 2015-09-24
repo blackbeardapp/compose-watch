00a3631e62b65a3172794fa114b5ef4f6ea6a4ab
Kevin Simper <kevin.simper@gmail.com>
2015-09-24 16:42:31 +0200

    Watch a different folder than the default which is the appname

--- cli.js
+++ cli.js
@@ -4,37 +4,44 @@ var path = require('path')
 var fs = require('fs')
 var debug = require('debug')('compose-watch')
 var argv = minimist(process.argv.slice(2), {
-  boolean: 'w'
+  boolean: ['w', 'h', 'help']
 })
 
-if(argv._.length !== 1) {
+console.log(argv)
+
+if(argv.h || argv.help) {
   var help = [
-    'Usage: compose-watch [-nw] appname',
+    'Usage: compose-watch [-nw] appname [dir]',
     '',
     'Options:',
-    ' -nw\tDo not watch, just restart the app once'
+    ' -h, --help\tOutput help',
+    ' -nw\tDo not watch, just restart the app once',
+    ' dir\tDir to watch if it should be different than the appname'
   ]
   console.log(help.join('\n'))
+  process.exit()
 }
 
-if(argv._.length === 1) {
-  var file = 'docker-compose.yml'
-  var dir = findFile('docker-compose.yml', process.cwd())
-  var app = argv._[0]
-  var composeYamlFile = dir + '/' + file
-  var commands = [
-    '--exec "',
-    'docker-compose -f ' + composeYamlFile + ' stop -t 0 ' + app + ' && ',
-    'docker-compose -f ' + composeYamlFile + ' start ' + app + ' && ',
-    'docker-compose -f ' + composeYamlFile + ' logs ' + app,
-    '"'
-  ]
-  if(!argv.nw) {
-    commands.push(' -w ' + dir + '/' + app + '/')
+var file = 'docker-compose.yml'
+var dir = findFile('docker-compose.yml', process.cwd())
+var app = argv._[0]
+var composeYamlFile = dir + '/' + file
+var commands = [
+  '--exec "',
+  'docker-compose -f ' + composeYamlFile + ' stop -t 0 ' + app + ' && ',
+  'docker-compose -f ' + composeYamlFile + ' start ' + app + ' && ',
+  'docker-compose -f ' + composeYamlFile + ' logs ' + app,
+  '"'
+]
+if(!argv.nw) {
+  var watchdir = app
+  if(typeof argv._[1] !== 'undefined') {
+    watchdir = argv._[1]
   }
-  debug('command', commands.join(''))
-  nodemon(commands.join(''))
+  commands.push(' -w ' + dir + '/' + watchdir + '/')
 }
+debug('command', commands.join(''))
+nodemon(commands.join(''))
 
 var prev = ''
 
